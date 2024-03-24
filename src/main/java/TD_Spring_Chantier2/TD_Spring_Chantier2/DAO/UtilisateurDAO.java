7c85b82e163c5edeeb83d03133e9bef35be92b7d
PLA <pierrelouis.adam@gmail.com>
2024-03-24 22:45:22 +0100

    Td_Spring_Chantier 2eme version

--- src/main/java/TD_Spring_Chantier2/TD_Spring_Chantier2/DAO/UtilisateurDAO.java
+++ src/main/java/TD_Spring_Chantier2/TD_Spring_Chantier2/DAO/UtilisateurDAO.java
@@ -1,10 +1,9 @@
 package TD_Spring_Chantier2.TD_Spring_Chantier2.DAO;
 
-import TD_Spring_Chantier2.TD_Spring_Chantier2.Model.Chantier;
-
+import TD_Spring_Chantier2.TD_Spring_Chantier2.Model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChantierDAO extends JpaRepository<Chantier, Integer> {
+public interface UtilisateurDAO extends JpaRepository<Utilisateur, Integer> {
 }

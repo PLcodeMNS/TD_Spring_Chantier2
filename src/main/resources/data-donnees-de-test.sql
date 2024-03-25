260339cd52eb88bea01130bfa3399bbff5e859bc
PLA <pierrelouis.adam@gmail.com>
2024-03-25 09:04:24 +0100

    Td_Spring_Chantier 2eme version

--- src/main/resources/data-donnees-de-test.sql
+++ src/main/resources/data-donnees-de-test.sql
@@ -1,6 +1,6 @@
 
 
-INSERT INTO Consommable (name)
+INSERT INTO consommable (name)
 VALUES ('Consommable 1');
 
 INSERT INTO consommable (name)
@@ -11,3 +11,6 @@ VALUES ('Consommable 3');
 
 INSERT INTO consommable (name)
 VALUES ('Consommable 4');
+
+INSERT INTO chantier (name, adresse)
+VALUES ('Chantier 1', '12 rue du chemin');
